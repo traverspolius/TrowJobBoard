933da7fa0212a42614a0a3de8e0dbe49ff1d9aab
Travers Polius <64287688+traverspolius@users.noreply.github.com>
2024-08-07 23:08:14 -0700

    Update helpers.php
    - two helper functions added.
    - inspect($value)
    - inspectAndDie($value)

--- helpers.php
+++ helpers.php
@@ -39,3 +39,29 @@
 		echo "Partial '{$name} not found!'";
 	}
  }
+ 
+ /**
+ * Inspect a value(s)
+ * @param string $value
+ * @return void
+ */
+ function inspect($value)
+ {
+	echo '<pre>';
+	var_dump($value);
+	echo '</pre>';
+ }
+ 
+ /**
+ * Inspect a value(s) and die
+ * @param string $value
+ * @return void
+ */
+ function inspectAndDie($value)
+ {
+	echo '<pre>';
+	die(var_dump($value));
+	echo '</pre>';
+ }
+ 
+ 
