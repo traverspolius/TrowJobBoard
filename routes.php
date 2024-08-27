a0db76fac40267299291724867fbcc4db6d714ea
Travers Polius <64287688+traverspolius@users.noreply.github.com>
2024-08-26 23:10:41 -0700

    Router.php
    Router class added

--- routes.php
+++ routes.php
@@ -1,8 +1,5 @@
 <?php
 
-return [
-    '/public/index.php' => 'controllers/home.php',
-    '/public/listings' => 'controllers/listings/index.php',
-    '/public/listings/create' => 'controllers/listings/create.php',
-    '404' => 'controllers/error/404.php'
-];
+$router->get('/public/index.php','controllers/home.php');
+$router->get('/public/listings','controllers/listings/index.php');
+$router->get('/public/listings/create','controllers/listings/create.php');
